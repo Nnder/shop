84c3e61cb97f7a147e5c3bb67283d44913c4f53a
Nnder <asfahaikrkt6k56@yandex.ru>
2024-03-25 01:30:23 +0500

    create Product Card and Products page

--- frontend/app/products/page.tsx
+++ frontend/app/products/page.tsx
@@ -1,11 +1,47 @@
-import {Container, Paper} from "@mui/material";
+import ProductCard from "@/src/4_features/Card/Card";
+import {Box, Container, Paper} from "@mui/material";
 import 'react-photo-view/dist/react-photo-view.css';
+
+const p = {
+  id: 1,
+  title: "Meat",
+  text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, eum blanditiis! Ipsa perferendis non corrupti molestias doloribus nesciunt, aliquam numquam eos quam, labore pariatur quidem ducimus ullam tempora eum provident! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, eum blanditiis! Ipsa perferendis non corrupti molestias doloribus nesciunt, aliquam numquam eos quam, labore pariatur quidem ducimus ullam tempora eum provident!",
+  imageSrc: "https://avatars.mds.yandex.net/i?id=8c1834ed9f9a1101dddc9b8050f7d4793ea0063e-5680793-images-thumbs&n=13",
+  imageTitle: "Meat",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, eum blanditiis! Ipsa perferendis non corrupti molestias doloribus nesciunt, aliquam numquam eos quam, labore pariatur quidem ducimus ullam tempora eum provident! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, eum blanditiis! Ipsa perferendis non corrupti molestias doloribus nesciunt, aliquam numquam eos quam, labore pariatur quidem ducimus ullam tempora eum provident!",
+}
 
 export default function About() {
   return (
    <Container>
     <Paper elevation={16} sx={{p: [1,2]}}>
-      
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        gap: [1,2,3,4],
+    }}>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+        <ProductCard product={p}/>
+      </Box>
     </Paper>
    </Container>
   );
---
+++ frontend/src/4_features/Card/Card.tsx
@@ -0,0 +1,47 @@
+"use client"
+import {Card, CardActionArea, CardActions, CardContent, CardMedia, Typography, Button} from '@mui/material';
+import { useRouter } from 'next/navigation';
+import { PropsWithChildren } from 'react';
+
+interface IProduct {
+    id: number
+    title: string
+    description: string
+    text: string
+    imageSrc: string
+    imageTitle: string
+}
+
+export default function ProductCard({product, ...props}: PropsWithChildren<{product: IProduct}> ) {
+    const router = useRouter()
+    const clickHandler = ()=> router.push(`/products/${product.id}`)
+    return (
+        <Card sx={{ maxWidth: ["90%", "45%", "30%", 330 ,330] }}>
+            <CardActionArea onClick={clickHandler}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image={product.imageSrc}
+                title={product.imageTitle}
+            />
+            </CardActionArea>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                {product.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    WebkitLineClamp: "4",
+                    WebkitBoxOrient: "vertical",
+                    fontSize: [14,14,14,18]
+                }}>
+                {product.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button sx={{width: "100%"}} size="large">Добавить в корзину</Button>
+            </CardActions>
+        </Card>
+  );
+}
