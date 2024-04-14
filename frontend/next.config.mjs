c08a63ed215ccc271e675ea6b4e13866c5b6d07e
nnder <asfahaikrkt6k56@yandex.ru>
2024-04-15 03:13:54 +0500

    again

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,9 +9,6 @@ const nextConfig = {
         YANDEX_CLIENT_SECRET: process.env.YANDEX_CLIENT_SECRET,
         YANDEX_APIKEY_MAP: process.env.YANDEX_APIKEY_MAP,
     },
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-    },
 };
 
 
--- frontend/src/4_features/Search/Search.tsx
+++ frontend/src/4_features/Search/Search.tsx
@@ -4,25 +4,25 @@ import Find from '../Find/Find'
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 export default function Search(setFind: any) {
-    const searchParams = useSearchParams();
+    // const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
 
-    useEffect(()=>{
-        setFind(searchParams.get('search') || "")
-    }, [])
+    // useEffect(()=>{
+    //     setFind(searchParams.get('search') || "")
+    // }, [])
 
     const ChangeHandler = (e: ChangeEvent<HTMLInputElement>)=>{
         const value = e.target.value
-        const params = new URLSearchParams(searchParams);
+        // const params = new URLSearchParams(searchParams);
         if(value){
             setFind(value)
-            params.set('search', value)
+            // params.set('search', value)
         } else {
             setFind("")
-            params.delete('search')
+            // params.delete('search')
         }
-        replace(`${pathname}?${params.toString()}`);
+        // replace(`${pathname}?${params.toString()}`);
     }
   return (
     <Find onChange={ChangeHandler}/>
